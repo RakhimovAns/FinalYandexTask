10bbc150bf9f0c04c454a8944f64ad8d3da7e51d
Ansar <ansarrakhimov91@gmail.com>
2024-04-10 22:58:48 +0300

    Added index.html and grpc service

--- cmd/main.go
+++ cmd/main.go
@@ -138,7 +138,8 @@ func Login(c *gin.Context) {
 func main() {
 	initializers.ConnectToDB()
 	initializers.CreateTable()
-	startGRPCServer()
+	go startGRPCServer()
+	startGinServer()
 }
 func startGRPCServer() {
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
@@ -155,16 +156,111 @@ func startGRPCServer() {
 }
 
 func startGinServer() {
+	log.Print("Start GIN SERVER")
 	r := gin.Default()
+	// Обработчик статических файлов
 	r.Static("/static", "./static")
+
+	// Обработчик для главной страницы
+	r.GET("/", func(c *gin.Context) {
+		c.File(filepath.Join("static", "index.html"))
+	})
 	r.GET("/reg", func(c *gin.Context) {
 		c.File(filepath.Join("static", "register.html"))
 	})
 	r.GET("/log", func(c *gin.Context) {
 		c.File(filepath.Join("static", "login.html"))
 	})
-	//	r.POST("/register", Register)
+	authorizedGroup := r.Group("/")
+	authorizedGroup.Use(initializers.Authorized())
+	{
+		// Обработчик POST-запроса на вычисление выражения
+		authorizedGroup.POST("/expression", PostExpression)
+
+		// Обработчик для получения статуса выражения
+		authorizedGroup.GET("/status/:id", GetStatus)
+	}
+
+	//Обработчик для регистрации
+	r.POST("/register", RegisterHandler)
 	r.POST("/login", Login)
 	r.POST("/logout", Logout)
 	r.Run()
+}
+
+func PostExpression(c *gin.Context) {
+	var expression models.Expression
+	if err := c.ShouldBindJSON(&expression); err != nil {
+		log.Printf(err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse JSON data: " + err.Error()})
+		return
+	}
+	conn, err := grpc.Dial(grpcServerURL, grpc.WithInsecure())
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed connect: " + err.Error()})
+		return
+	}
+	defer conn.Close()
+	client := desc.NewCalculusClient(conn)
+	resp, err := client.Calculate(context.Background(), &desc.Expression{Expression: expression.Expression, AddTime: expression.AddTime, DivideTime: expression.DivideTime, SubTime: expression.SubTime, MultiplyTime: expression.MultiplyTime})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to get user info from gRPC server " + err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"id": resp.Id})
+}
+
+func GetStatus(c *gin.Context) {
+	id := c.Param("id")
+	ID, _ := strconv.Atoi(id)
+	conn, err := grpc.Dial(grpcServerURL, grpc.WithInsecure())
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed connect: " + err.Error()})
+		return
+	}
+	defer conn.Close()
+	client := desc.NewCalculusClient(conn)
+	_, err = client.Status(context.Background(), &desc.ID{Id: int64(ID)})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to get user info from gRPC server " + err.Error()})
+		return
+	}
+	expression := initializers.GetByID(int64(ID))
+	if expression.IsCounted {
+		c.JSON(http.StatusOK, gin.H{"result": expression.Result})
+	} else {
+		// Отправляем сообщение о том, что подсчет начат
+		c.JSON(http.StatusOK, gin.H{"result": "counting"})
+		// Запускаем горутину для асинхронного подсчета выражения
+		go func() {
+			_, err := service.CountExpression(expression)
+			if err != nil {
+				log.Printf("Error counting expression: %v", err)
+				return
+			}
+		}()
+	}
+}
+
+func RegisterHandler(c *gin.Context) {
+	var user models.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		log.Printf(err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse user data: " + err.Error()})
+		return
+	}
+	conn, err := grpc.Dial(grpcServerURL, grpc.WithInsecure())
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed connect: " + err.Error()})
+		return
+	}
+	defer conn.Close()
+	client := desc.NewCalculusClient(conn)
+	_, err = client.Register(context.Background(), &desc.User{Name: user.Name, Password: user.Password})
+	if err != nil {
+		log.Printf(err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to register user: " + err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"result": "registered successfully, try to log in"})
 }
